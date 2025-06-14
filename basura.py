e8e6d9a6bb57c66ba80e8c21b8c036cd68c487e3
M-Olson-start <miriamlorenaolson@gmail.com>
2025-06-14 19:19:00 -0300

    correcciones hechas hasta las 16:30

---
+++ basura.py
@@ -0,0 +1,34 @@
+# def predecir_naive_bayes(fila, conteo_clases, conteo_atributos, total_datos):
+#     probabilidades = {}
+#     #fila : una nueva observación sin la clase (ej: ['Edad', 'Sexo', '?'])
+#     #conteo_clases  : dict con el total de ejemplos por clase
+#     #conteo_atributos : dict con los conteos de atributos por clase
+#     #total_datos : total de filas en el conjunto de entrenamiento
+
+#     #calcula la probabilidad de que la fila pertenezca a cada clase y devuelve 
+#     # la clase más probable (y también todas las probabilidades si querés verlas).
+#     for clase in conteo_clases:
+#         probabilidad_clase = conteo_clases[clase] / total_datos
+#         probabilidad_atributos = probabilidad_clase
+
+#         for i, atributo in enumerate(fila[:-1]):
+#             if atributo in conteo_atributos[clase][i]:
+#                 probabilidad_atributos *= conteo_atributos[clase][i][atributo] / conteo_clases[clase]
+#                # print("\nif fila",conteo_atributos[clase][i],"valor ",conteo_atributos[clase][i][atributo])
+#             else:
+#                 probabilidad_atributos *= 0.0001  # Suavizado para evitar probabilidad 0
+#                # print("\nelse fila",conteo_atributos[clase][i])
+
+#         probabilidades[clase] = probabilidad_atributos
+   
+#     return max(probabilidades, key=probabilidades.get), probabilidades
+
+
+
+ # fila : nueva observación (sin clase), ej: ['Adulto', 'M']
+    # conteo_clases : dict con total de ejemplos por clase, ej: {'Si': 5, 'No': 3}
+    # conteo_atributos : dict con conteos de atributos por clase, estructura:
+    #    {'Si': {0: {'Joven': 2, 'Adulto': 3}, 1: {'M': 1, 'F': 4}}, ...}
+    # total_datos : total de filas de entrenamiento
+    # valores_posibles : dict que indica cuántos valores posibles tiene cada atributo:
+    #    ej: {0: ['Joven', 'Adulto', 'Mayor'], 1: ['M', 'F']}
--- funciones.py
+++ funciones.py
@@ -15,56 +15,55 @@ def find_s(examples):
 
     return hypothesis
 
-def aplicarHipotesis(examples, hypothesis):
-    contador = 0
-    for example in examples:
+def aplicarHipotesis(datosDePrueba, hypothesis):
+    prediccion = 0
+    for dato in datosDePrueba:
         cumple = True
         for i in range(len(hypothesis)):
-            if hypothesis[i] != '?' and hypothesis[i] != example[i]:
+            if hypothesis[i] != '?' and hypothesis[i] != dato[i]:
                 cumple = False
                 break
         if cumple:
-            contador += 1
-            print("[",example[0],"|",example[1],"|",example[2],"|",example[3],"|",example[4],"|",example[5],"|","OTORGADO ]" )
-      # else: 
-            #print("NO CUMPLE", example)
-    return contador
+            prediccion += 1
+            print("[",dato[0],"|",dato[1],"|",dato[2],"|",dato[3],"|",dato[4],"|",dato[5],"|","OTORGADO ]" )
+      
+    return prediccion
 
-def evaluar_hipotesis(hypothesis, datos_prueba):
+def calcularMatrizDeConfusion(dataSet,hypothesis ):
     TP = FP = TN = FN = 0
 
-    for ejemplo in datos_prueba:
-        verdadero_estado = ejemplo[-1]  # Última columna es la etiqueta real
+    for persona in dataSet:
+        estadoPrestamo = persona[-1]  # Última columna es la etiqueta real
         prediccion = "OTORGADO"
         for i in range(len(hypothesis)):
-            if hypothesis[i] != "?" and hypothesis[i] != ejemplo[i]:
+            if hypothesis[i] != "?" and hypothesis[i] != persona[i]:
                 prediccion = "RECHAZADO"
                 break
 
         if prediccion == "OTORGADO":
-            if verdadero_estado == "OTORGADO":
+            if estadoPrestamo == "OTORGADO":
                 TP += 1
             else:
                 FP += 1
         else:
-            if verdadero_estado == "RECHAZADO":
+            if estadoPrestamo == "RECHAZADO":
                 TN += 1
             else:
                 FN += 1
-
-    return TP, FP, TN, FN
-
-
-def metricas (TP,FP,TN,FN):
     print("\nMATRIZ DE CONFUSION")
     print(f"TP (TRUE POSITIVE): {TP}")
     print(f"FP (FALSE POSITIVE): {FP}")
     print(f"TN (TRUE NEGATIVE): {TN}")
     print(f"FN (FALSE NEGATIVE): {FN}")
-    print(" ",TP,  "|", FN , "\n ",FP ,"|" ,TN )
+    print(" ",TP,  "|", FN , "\n ",FP ,"|" ,TN ) 
+
+    return TP, FP, TN, FN
+
+
+def calcularMetricas (TP,FP,TN,FN):
 
     # Cálculos de métricas
-    total = TP + FP + TN + FN
+    total = TP + FP + TN + FN # esta bien
     accuracy = (TP + TN) / total
     recall = TP / (TP + FN) if TP + FN > 0 else 0
     especificidad = TN / (TN + FP) if TN + FP > 0 else 0
@@ -108,62 +107,32 @@ def entrenar_naive_bayes(datos_entrenamiento):
     conteo_clases = {}
     conteo_atributos = {}
 
-    for fila in datos_entrenamiento:
-        clase = fila[-1]
+    for persona in datos_entrenamiento:
+        clase = persona[-1]
         if clase not in conteo_clases:
             conteo_clases[clase] = 0   #inicializa cada uno de los conjuntos de atributos
-            conteo_atributos[clase] = [{} for _ in range(len(fila) - 1)]
+            conteo_atributos[clase] = [{} for _ in range(len(persona) - 1)]
         conteo_clases[clase] += 1
 
-        for i, atributo in enumerate(fila[:-1]): #recorre cada fila 
+        for i, atributo in enumerate(persona[:-1]): #recorre cada fila 
             if atributo not in conteo_atributos[clase][i]:# inicializa el conteo del atributo si corresponde
                 conteo_atributos[clase][i][atributo] = 0
             conteo_atributos[clase][i][atributo] += 1 # cuenta la cantidad de veces que aparece dicho atributo
     #print("conteo_clases: ",conteo_clases, "\nconteo_atributos: ", conteo_atributos)
     return conteo_clases, conteo_atributos
 
-# def predecir_naive_bayes(fila, conteo_clases, conteo_atributos, total_datos):
-#     probabilidades = {}
-#     #fila : una nueva observación sin la clase (ej: ['Edad', 'Sexo', '?'])
-#     #conteo_clases  : dict con el total de ejemplos por clase
-#     #conteo_atributos : dict con los conteos de atributos por clase
-#     #total_datos : total de filas en el conjunto de entrenamiento
 
-#     #calcula la probabilidad de que la fila pertenezca a cada clase y devuelve 
-#     # la clase más probable (y también todas las probabilidades si querés verlas).
-#     for clase in conteo_clases:
-#         probabilidad_clase = conteo_clases[clase] / total_datos
-#         probabilidad_atributos = probabilidad_clase
 
-#         for i, atributo in enumerate(fila[:-1]):
-#             if atributo in conteo_atributos[clase][i]:
-#                 probabilidad_atributos *= conteo_atributos[clase][i][atributo] / conteo_clases[clase]
-#                # print("\nif fila",conteo_atributos[clase][i],"valor ",conteo_atributos[clase][i][atributo])
-#             else:
-#                 probabilidad_atributos *= 0.0001  # Suavizado para evitar probabilidad 0
-#                # print("\nelse fila",conteo_atributos[clase][i])
-
-#         probabilidades[clase] = probabilidad_atributos
-   
-#     return max(probabilidades, key=probabilidades.get), probabilidades
-
-def predecir_naive_bayes(fila, conteo_clases, conteo_atributos, total_datos, valores_posibles):
+def predecir_naive_bayes(persona, conteo_clases, conteo_atributos, total_datos, valores_posibles):
     probabilidades = {}
 
-    # fila : nueva observación (sin clase), ej: ['Adulto', 'M']
-    # conteo_clases : dict con total de ejemplos por clase, ej: {'Si': 5, 'No': 3}
-    # conteo_atributos : dict con conteos de atributos por clase, estructura:
-    #    {'Si': {0: {'Joven': 2, 'Adulto': 3}, 1: {'M': 1, 'F': 4}}, ...}
-    # total_datos : total de filas de entrenamiento
-    # valores_posibles : dict que indica cuántos valores posibles tiene cada atributo:
-    #    ej: {0: ['Joven', 'Adulto', 'Mayor'], 1: ['M', 'F']}
-
+   
     for clase in conteo_clases:
         # P(clase)
         probabilidad_clase = conteo_clases[clase] / total_datos
         probabilidad_total = probabilidad_clase
 
-        for i, atributo in enumerate(fila[:-1]):
+        for i, atributo in enumerate(persona[:-1]):
             posibles_valores = len(valores_posibles[i])  # k en el denominador
             conteo_valor = conteo_atributos[clase][i].get(atributo, 0)
 
@@ -196,3 +165,5 @@ def calcular_curva_roc(y_true, y_scores):
         fpr.append(FP / (FP + TN) if FP + TN > 0 else 0)
 
     return fpr, tpr
+
+    
